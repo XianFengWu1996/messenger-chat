74b3befa16a06e57a185e53c477585bbebff6946
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-05-12 23:35:17 -0400

    create signin page layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
+
+        <AuthForm />
       </div>
     </div>
   );
